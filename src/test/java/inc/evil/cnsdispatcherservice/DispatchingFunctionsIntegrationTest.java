0c6880c42777f66615d22b4668fb4290dd110d73
Ion Pascari <ionpascari1995@gmail.com>
2023-07-12 20:44:07 +0300

    feat: fix failing test

--- src/test/java/inc/evil/cnsdispatcherservice/DispatchingFunctionsIntegrationTest.java
+++ src/test/java/inc/evil/cnsdispatcherservice/DispatchingFunctionsIntegrationTest.java
@@ -1,5 +1,6 @@
 package inc.evil.cnsdispatcherservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
@@ -9,13 +10,33 @@ import reactor.test.StepVerifier;
 
 import java.util.function.Function;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @FunctionalSpringBootTest
+@Disabled("These tests are only necessary when using the functions alone (no bindings)")
 class DispatchingFunctionsIntegrationTest {
 
     @Autowired
     private FunctionCatalog functionCatalog;
+
+    @Test
+    void packOrder() {
+        Function<OrderAcceptedMessage, Long> pack = functionCatalog.lookup(Function.class, "pack");
+        long orderId = 121;
+        assertThat(pack.apply(new OrderAcceptedMessage(orderId))).isEqualTo(orderId);
+    }
+
+    @Test
+    void labelOrder() {
+        Function<Flux<Long>, Flux<OrderDispatchedMessage>> label = functionCatalog.lookup(Function.class, "label");
+        Flux<Long> orderId = Flux.just(121L);
+
+        StepVerifier.create(label.apply(orderId))
+                .expectNextMatches(dispatchedOrder ->
+                        dispatchedOrder.equals(new OrderDispatchedMessage(121L)))
+                .verifyComplete();
+    }
 
     @Test
     void packAndLabel() {
